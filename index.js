835c1cc5aac639b4452717e68fb3840141849975
Casper <me@casperengelmann.com>
2020-09-19 17:58:08 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    quotes: 'single',
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
+    semi: [2, 'never'],
+    'comma-dangle': [2, 'always-multiline'],
+    'object-curly-newline': [2, {
+      multiline: true,
+      minProperties: 3,
+    }],
+    'array-bracket-spacing': [2, 'never'],
+    'object-curly-spacing': [2, 'always'],
+  },
+};
